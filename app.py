bf25fb5bb92af92698be20485a51159283be2356
abubakarmsese18-lang <abubakar.msese18@student.iiu.edu.pk>
2026-05-19 23:34:28 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+
+import streamlit as st
+import pandas as pd
+import datetime
+from PIL import Image
+import plotly.express as px
+import plotly.graph objects as go
+
+
+df = pd.read_csv("abu.csv")
+st.set_page_config(layout="wide")
+st.markdown ("<systale>div.block-container{padding-top:10px;]}")
+
+
